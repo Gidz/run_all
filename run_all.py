bab1cea2413e8733ea3743b03c2e8028ba2407f0
Gideon Paul <gidzpaul95@gmail.com>
2019-02-20 16:05:44 -0500

    Safety checks and comments

--- run_all.py
+++ run_all.py
@@ -1,14 +1,14 @@
 import os
 import subprocess
 
-## Shared variables
+#All the files that can't be run will be appended to this list
 unknown_files = []
-## End of shared variables
 
-#Extension vs Executable map
+#Association map
 # To support more types of scripts,
 #Simply add the extension and which shell command will execute the extension here
-extensions = {
+associations = {
+  # "file extension":"will be run by",
   "py":"python",
   "pl":"perl",
   "sh":"sh",
@@ -22,21 +22,34 @@ def _run(executable,file):
     print("")
 
 def run(files_list):
+    # Keep count of the number of scripts executed
     count = 0
     for file in files_list:
+        # This script will be executed as well since it is in the directory.
+        # Preventing this by a simple check.
         if not __file__==file:
-            temp = file.split('.')
-            if(len(temp)!=1):
-                executable = extensions[temp[-1]]
-                _run(executable, file)
-                count = count + 1
+            name_and_extension_list= file.split('.')
+            if(len(name_and_extension_list)!=1):
+                extension=name_and_extension_list[-1]
+                executable = associations.get(extension,None)
+
+                # If the extension has not associated runner, add it to the unknown files list
+                if not executable:
+                    unknown_files.append(file)
+                else:
+                    _run(executable, file)
+                    count = count + 1
             else:
+                # Add the files without an extension to unknown_files
                 unknown_files.append(file)
     return count
 
 def main():
      num_executed= run(os.listdir())
-     print("Executed",num_executed,"script(s)")
+     print("\nUnable to execute",len(unknown_files),"script(s).")
+     for file in unknown_files:
+         print('>', file)
+     print("\nExecuted",num_executed,"script(s)")
 
 if __name__=="__main__":
     main()
